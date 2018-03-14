c31ba89cd988964dbfa3ec052b3446f70c50f1d9
Jason Lyles <lylesjt@gmail.com>
2018-03-14 14:31:22 -0400

    Adding fog-aws gem

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,12 +5,12 @@ CarrierWave.configure do |config|
   elsif Rails.env.development?
     config.storage = :file
   else
+    config.storage = :fog
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['S3_KEY'],
       aws_secret_access_key: ENV['S3_SECRET']
     }
-    config.storage = :fog
     config.fog_directory = 'gotkayak-images'
     config.fog_host = 'https://s3.amazonaws.com/gotkayak-images'
     config.fog_public = true
