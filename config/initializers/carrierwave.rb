a0fbfbcc1e4e284094db3dfb35adcf8c4bba4bf4
Jason Lyles <lylesjt@gmail.com>
2018-03-14 14:41:33 -0400

    More reworking carrierwave config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,3 +1,5 @@
+require 'fog/aws'
+
 CarrierWave.configure do |config|
   if Rails.env.test?
     config.storage = :file
@@ -5,13 +7,13 @@ CarrierWave.configure do |config|
   elsif Rails.env.development?
     config.storage = :file
   else
-    config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['S3_KEY'],
       aws_secret_access_key: ENV['S3_SECRET']
     }
+    config.storage = :fog
     config.fog_directory = 'gotkayak-images'
     config.fog_host = 'https://s3.amazonaws.com/gotkayak-images'
     config.fog_public = true
